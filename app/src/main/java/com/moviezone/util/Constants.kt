d9d189fa858793a36dbc89425d8d0943b76a22df
ahzamee <ahzamee@gmail.com>
2022-08-19 19:02:39 +0600

    fix: Constant file modified

--- app/src/main/java/com/moviezone/util/Constants.kt
+++ app/src/main/java/com/moviezone/util/Constants.kt
@@ -3,5 +3,10 @@ package com.moviezone.util
 object Constants {
 
     const val BaseUrl = "https://www.omdbapi.com"
+<<<<<<< HEAD
     const val ApiKey = "";
 }
+=======
+    const val ApiKey = "your_key";
+}
+>>>>>>> be1239c (fix: Constant file modified)
